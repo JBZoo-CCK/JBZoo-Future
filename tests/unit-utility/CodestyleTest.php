3d5a4bf42af795eb60b5c3aecbe3d9c8a21b4043
SmetDenis <denis@jbzoo.com>
2016-03-16 19:46:18 +0600

    stable version of package

---
+++ tests/unit-utility/CodestyleTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * JBZoo CCK
+ *
+ * This file is part of the JBZoo CCK package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package   CCK
+ * @license   MIT
+ * @copyright Copyright (C) JBZoo.com,  All rights reserved.
+ */
+
+namespace JBZoo\PHPUnit;
+
+/**
+ * Class CodeStyleTest
+ * @package JBZoo\PHPUnit
+ */
+class CodeStyleTest extends Codestyle
+{
+    protected $_packageName = 'CCK';
+    protected $_packageLicense = 'Proprietary http://jbzoo.com/license';
+}
