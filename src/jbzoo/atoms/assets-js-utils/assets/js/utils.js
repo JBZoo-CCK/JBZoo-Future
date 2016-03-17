48757c5c7917cb47f388b51201c54a5562ac4cd7
SmetDenis <denis@jbzoo.com>
2016-03-17 20:13:08 +0600

    Check CSS, JS, LESS and XML copyrights

--- src/jbzoo/atoms/assets-js-utils/assets/js/utils.js
+++ src/jbzoo/atoms/assets-js-utils/assets/js/utils.js
@@ -1,15 +1,14 @@
 /**
- * JBZoo Helper
+ * JBZoo CCK
  *
  * This file is part of the JBZoo CCK package.
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @package   Utils
- * @license   MIT
- * @copyright Copyright (C) JBZoo.com,  All rights reserved.
- * @link      https://github.com/JBZoo/Utils
- * @author    Denis Smetannikov <denis@jbzoo.com>
+ * @package    CCK
+ * @license    Proprietary http://jbzoo.com/license
+ * @copyright  Copyright (C) JBZoo.com,  All rights reserved.
+ * @link       http://jbzoo.com
  */
 
 /**
