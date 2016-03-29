28e205a6a8446d1778bfe9b2c78f7bfc3c286f1f
SmetDenis <denis@jbzoo.com>
2016-03-29 20:53:26 +0700

    скрипты для реакта

--- src/jbzoo/atoms/assets-react/assets-react.php
+++ src/jbzoo/atoms/assets-react/assets-react.php
@@ -12,16 +12,43 @@
  * @link       http://jbzoo.com
  */
 
+use JBZoo\Assets\Asset\Asset;
 use JBZoo\CCK\App;
 
 return [
     'init' => function (App $app) {
+
+        $list = [
+            'atom-assets-react:assets\js\react.min.js',
+            'atom-assets-react:assets\js\react-dom.min.js',
+        ];
+
+        if (0 && $app->isDebug()) {
+            $list = [
+                'https://cdnjs.cloudflare.com/ajax/libs/react/0.14.7/react.js',
+                'https://cdnjs.cloudflare.com/ajax/libs/react/0.14.7/react-with-addons.js',
+                'https://cdnjs.cloudflare.com/ajax/libs/react/0.14.7/react-dom.js',
+            ];
+        }
+
         $app['assets']->register(
-            'react',
-            [
-                'atom-assets-react:assets\js\react.min.js',
-                'atom-assets-react:assets\js\react-dom.min.js',
-            ]
+            'react-fonts',
+            'var WebFontConfig = {
+                google: { families: [ \'Roboto:400,300,500:latin\' ] }
+            };
+            (function() {
+                var wf = document.createElement(\'script\');
+                wf.src = (\'https:\' == document.location.protocol ? \'https\' : \'http\') +
+                \'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js\';
+                wf.type = \'text/javascript\';
+                wf.async = \'true\';
+                var s = document.getElementsByTagName(\'script\')[0];
+                s.parentNode.insertBefore(wf, s);
+            })();',
+            [],
+            ['type' => Asset::TYPE_JS_CODE]
         );
+
+        $app['assets']->register('react', $list, ['react-fonts']);
     },
 ];
