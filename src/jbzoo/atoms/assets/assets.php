2ef98705dfd8287832cabd3d53bb0c94ea0d8263
SmetDenis <denis@jbzoo.com>
2016-03-29 20:50:55 +0700

    правки подключения статики

--- src/jbzoo/atoms/assets/assets.php
+++ src/jbzoo/atoms/assets/assets.php
@@ -12,6 +12,7 @@
  * @link       http://jbzoo.com
  */
 
+use JBZoo\Assets\Asset\Asset;
 use JBZoo\CCK\App;
 use JBZoo\Utils\FS;
 use JBZoo\Utils\Url;
@@ -20,41 +21,81 @@ return [
 
     'events' => [
 
-        // TODO: Add cutsom filter for Assets helepr
+        // TODO: Add cutsom filters for Assets helper (join, compress, mdata, resolver, path rewriter)
         'jbzoo.assets' => function (App $app) {
-
             $app->trigger('atom.assets.header.before');
 
-            $rootPath = $app['path']->getRoot();
+            // Vars
+            $echoResult  = [];
+            $rootJBZoo   = $app['path']->get('jbzoo:');
+            $rootGlobal  = $app['path']->get('root:');
+            $resolvePath = function ($fullPath) use ($rootJBZoo, $rootGlobal) {
+
+                if (!Url::isAbsolute($fullPath)) {
+
+                    $mtime = substr(filemtime($fullPath), -3);
+
+                    $relPath = FS::getRelative($fullPath, $rootJBZoo, '/', false);
+
+                    if (FS::clean($relPath) !== FS::clean($fullPath)) {
+                        $fullPath = Url::buildAll(Url::root(), [
+                            'path'  => '/' . JBZOO_EXT_PATH . '/' . $relPath,
+                            'query' => 'mtime=' . $mtime,
+                        ]);
+                    } else {
+
+                        $relPath  = FS::getRelative($fullPath, $rootGlobal, '/', true);
+                        $fullPath = Url::buildAll(Url::root(), [
+                            'path'  => '/' . $relPath,
+                            'query' => 'mtime=' . $mtime,
+                        ]);
+                    }
+                }
+
+                return $fullPath;
+            };
 
             // Resolve all dependencies for registered assets
             $list = $app['assets']->build();
+            $app->trigger('atom.assets.build');
+
 
             // Include styles
-            foreach ($list['css'] as $fullPath) {
-                if (!Url::isAbsolute($fullPath)) {
-                    $file     = FS::getRelative($fullPath, $rootPath, '/', false);
-                    $fullPath = Url::root() . '/administrator/components/com_jbzoo/' . $file;
-                }
-
-                $app['header']->cssFile($fullPath);
+            foreach ($list[Asset::TYPE_CSS_FILE] as $fullPath) {
+                $app['header']->cssFile($resolvePath($fullPath));
             }
 
             // Include JS-scripts
-            foreach ($list['js'] as $fullPath) {
-
-                if (!Url::isAbsolute($fullPath)) {
-                    $relatedPath = FS::getRelative($fullPath, $app['path']->get('jbzoo:'), '/', false);
-                    $fullPath    = Url::buildAll(Url::root(), ['path' => '/' . JBZOO_EXT_PATH . '/' . $relatedPath]);
-                }
-
+            foreach ($list[Asset::TYPE_JS_FILE] as $fullPath) {
                 if ($app['env']->isAdmin()) {
-                    echo '<script src="' . $fullPath . '" type="text/javascript"></script>' . PHP_EOL;
+                    $echoResult[] = '<script type="text/javascript" src="' . $resolvePath($fullPath) . '"></script>';
                 } else {
-                    $app['header']->jsFile($fullPath);
+                    $app['header']->jsFile($resolvePath($fullPath));
                 }
-
             }
+
+            // Include JSX-scripts
+            foreach ($list[Asset::TYPE_JSX_FILE] as $fullPath) {
+                $echoResult[] = '<script type="text/babel" src="' . $resolvePath($fullPath) . '"></script>';
+            }
+
+            // Print plain CSS code
+            foreach ($list[Asset::TYPE_CSS_CODE] as $cssCode) {
+                $app['header']->cssCode($cssCode);
+            }
+
+            // Print plain JS code
+            foreach ($list[Asset::TYPE_JS_CODE] as $jsCode) {
+                $app['header']->jsCode($jsCode);
+            }
+
+            // Print plain JSX code
+            foreach ($list[Asset::TYPE_JSX_CODE] as $jsxCode) {
+                $echoResult[] = sprintf('<script type="text/babel">%s</script>', $jsxCode);
+            }
+
+            // TODO: Replace echo to CrossCMS helper
+            echo PHP_EOL . PHP_EOL . implode(PHP_EOL, $echoResult) . PHP_EOL;
 
             $app->trigger('atom.assets.header.after');
         },
