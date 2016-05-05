8dd968a8b9fefe773ae7e1e610529448724f0e84
SmetDenis <denis@jbzoo.com>
2016-05-05 17:46:44 +0700

    инсталлер joomla

---
+++ src/joomla-plugins/com_jbzoo_admin/script.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * JBZoo CCK
+ *
+ * This file is part of the JBZoo CCK package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package    CCK
+ * @license    Proprietary http://jbzoo.com/license
+ * @copyright  Copyright (C) JBZoo.com,  All rights reserved.
+ * @link       http://jbzoo.com
+ */
+
+use JBZoo\CCK\App;
+
+/**
+ * Class com_jbzooInstallerScript
+ */
+class com_jbzooInstallerScript
+{
+    /**
+     * Init JBZoo framework
+     */
+    protected function _loadJBZoo()
+    {
+        if ($initFile = realpath(__DIR__ . '/jbzoo/init.php')) {
+            require_once $initFile;
+        }
+    }
+
+    /**
+     * Run install script
+     */
+    public function install()
+    {
+        $this->_loadJBZoo();
+
+        $app = App::getInstance();
+        $app['core.installer']->install();
+    }
+
+    /**
+     * Run uninstall script
+     */
+    public function uninstall()
+    {
+        $this->_loadJBZoo();
+
+        $app = App::getInstance();
+        $app['core.installer']->uninstall();
+    }
+
+    /**
+     * Run update script
+     */
+    public function update()
+    {
+        $this->_loadJBZoo();
+
+        $app = App::getInstance();
+        $app['core.installer']->update();
+    }
+}
