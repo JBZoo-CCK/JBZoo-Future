9af456fbeddc9bf19d3dd641d8ce5fba53df1241
smetdenis <admin@jbzoo.com>
2016-05-07 18:50:21 +0700

    скрипт запуска всех тестов

---
+++ bin/jbzoo-test.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+echo ""
+echo ">>> >>> PHPUnit Joomla"
+./bin/phpunit   --configuration ./phpunit-joomla.xml.dist
+
+echo ""
+echo ">>> >>> PHPUnit Wordpress"
+./bin/phpunit   --configuration ./phpunit-wordpress.xml.dist
+
+echo ""
+echo ">>> >>> PHPUnit Utility"
+./bin/phpunit   --configuration ./phpunit-utility.xml.dist      ./tests/unit/utility/CodeStyleTest.php
+
+echo ""
+echo ">>> >>> PHP Statistics"
+./bin/phpmd  ./src/jbzoo                            \
+    text                                            \
+    ./src/jbzoo/vendor/jbzoo/misc/phpmd/jbzoo.xml   \
+    --verbose
+
+./bin/phpcs  ./src/jbzoo                                                \
+    --extensions=php                                                    \
+    --standard=./src/jbzoo/vendor/jbzoo/misc/phpcs/JBZoo/ruleset.xml    \
+    --report=full
+
+./bin/phpcpd ./src/jbzoo --verbose
+
+./bin/phploc ./src/jbzoo --verbose
