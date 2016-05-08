a348e1c47fe40c30cf16c0490b067689da9c9861
smetdenis <admin@jbzoo.com>
2016-05-08 20:08:22 +0700

    новые версии либ

--- bin/jbzoo-test.sh
+++ bin/jbzoo-test.sh
@@ -2,28 +2,28 @@
 
 echo ""
 echo ">>> >>> PHPUnit Joomla"
-./bin/phpunit   --configuration ./phpunit-joomla.xml.dist
+sh ./bin/phpunit   --configuration ./phpunit-joomla.xml.dist
 
 echo ""
 echo ">>> >>> PHPUnit Wordpress"
-./bin/phpunit   --configuration ./phpunit-wordpress.xml.dist
+sh ./bin/phpunit   --configuration ./phpunit-wordpress.xml.dist
 
 echo ""
 echo ">>> >>> PHPUnit Utility"
-./bin/phpunit   --configuration ./phpunit-utility.xml.dist      ./tests/unit/utility/CodeStyleTest.php
+sh ./bin/phpunit   --configuration ./phpunit-utility.xml.dist      ./tests/unit/utility/CodeStyleTest.php
 
 echo ""
 echo ">>> >>> PHP Statistics"
-./bin/phpmd  ./src/jbzoo                            \
+sh ./bin/phpmd  ./src/jbzoo                            \
     text                                            \
     ./src/jbzoo/vendor/jbzoo/misc/phpmd/jbzoo.xml   \
     --verbose
 
-./bin/phpcs  ./src/jbzoo                                                \
+sh ./bin/phpcs  ./src/jbzoo                                                \
     --extensions=php                                                    \
     --standard=./src/jbzoo/vendor/jbzoo/misc/phpcs/JBZoo/ruleset.xml    \
     --report=full
 
-./bin/phpcpd ./src/jbzoo
+sh ./bin/phpcpd ./src/jbzoo
 
-./bin/phploc ./src/jbzoo --verbose
+sh ./bin/phploc ./src/jbzoo --verbose
