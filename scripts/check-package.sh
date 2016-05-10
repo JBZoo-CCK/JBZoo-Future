a8488130e23709520425e16f072d336499e71c4e
SmetDenis <denis@jbzoo.com>
2016-05-10 15:13:56 +0700

    Версии bower

--- scripts/check-package.sh
+++ scripts/check-package.sh
@@ -1,24 +1,11 @@
 #!/usr/bin/env sh
 
-echo ""
-echo ">>> >>> NPM: Cleanup"
-rm -fr ./node_modules
-echo ">>> >>> NPM: Install"
-NODE_ENV=development npm install
+echo "- - -"
+echo "-= Check ALL =- "
+echo "- - -"
 
-
-echo ""
-echo ">>> >>> Bower: Cleanup"
 rm -fr ./bower_components
-echo ">>> >>> Bower: Update"
 NODE_ENV=development ./node_modules/.bin/bower update
-
-
-echo ""
-echo ">>> >>> Gulp: Update"
 NODE_ENV=development ./node_modules/.bin/gulp update
-
-
-echo ""
-echo ">>> >>> Webpack"
 NODE_ENV=development ./node_modules/.bin/webpack -v
+./node_modules/.bin/webpack -v
