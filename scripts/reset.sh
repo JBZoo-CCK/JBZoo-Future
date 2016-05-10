116af1c57d9feccb6ebd21fba0cdbf7c9b12d4e5
SmetDenis <denis@jbzoo.com>
2016-05-10 11:28:55 +0700

    Скрипт сброса

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -3,3 +3,11 @@
 echo ""
 echo ">>> >>> GIT"
 git reset --hard
+
+echo ""
+echo ">>> >>> Remove dirs"
+rm -fr ./src/jbzoo/vendor
+rm -fr ./vendor
+rm -fr ./bin
+rm -fr ./node_modules
+rm -fr ./bower_components
