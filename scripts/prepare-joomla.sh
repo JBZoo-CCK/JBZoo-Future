a750bac9093d8ccbc35ff9e15ba3c7cd2b318932
SmetDenis <denis@jbzoo.com>
2016-05-10 19:00:32 +0700

    sh copyrights

--- scripts/prepare-joomla.sh
+++ scripts/prepare-joomla.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env sh
 
+#
+# JBZoo CCK
+#
+# This file is part of the JBZoo CCK package.
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+# @package   CCK
+# @license   Proprietary http://jbzoo.com/license
+# @copyright Copyright (C) JBZoo.com,  All rights reserved.
+# @link      http://jbzoo.com
+#
+
 SITE_NAME="cck-joomla"
 SITE_WWW="resources"
 
--- scripts/update-dev.sh
+++ scripts/update-dev.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env sh
 
+#
+# JBZoo CCK
+#
+# This file is part of the JBZoo CCK package.
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+# @package   CCK
+# @license   Proprietary http://jbzoo.com/license
+# @copyright Copyright (C) JBZoo.com,  All rights reserved.
+# @link      http://jbzoo.com
+#
+
 echo "-= Update Project for Developing =- "
 echo ">>> >>> Prepare paths"
 mkdir -p ./build/clover_xml
