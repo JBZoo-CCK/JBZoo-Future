e2daffb6a03b7bfbf193f3942a6239006c852073
SmetDenis <denis@jbzoo.com>
2016-05-10 17:38:58 +0700

    фиксы путей для симлинков

--- scripts/prepare-tests.sh
+++ scripts/prepare-tests.sh
@@ -29,22 +29,22 @@ composer update                 \
 
 echo ""
 echo ">>> >>> Joomla: Create symlinks"
-rm -r `$JOOMLA`/plugins/system/jbzoocck
-ln -s `$ROOT`/src/joomla-plugins/plg_sys_jbzoocck                           \
-      `$JOOMLA`/plugins/system/jbzoocck
+rm -r "$JOOMLA/plugins/system/jbzoocck"
+ln -s "$ROOT/src/joomla-plugins/plg_sys_jbzoocck"                           \
+      "$JOOMLA/plugins/system/jbzoocck"
 
 
-rm -r `$JOOMLA`/administrator/components/com_jbzoo
-ln -s `$ROOT`/src/joomla-plugins/pkg_jbzoocck/packages/com_jbzoo/admin      \
-      `$JOOMLA`/administrator/components/com_jbzoo
+rm -r "$JOOMLA/administrator/components/com_jbzoo"
+ln -s "$ROOT/src/joomla-plugins/pkg_jbzoocck/packages/com_jbzoo/admin"      \
+      "$JOOMLA/administrator/components/com_jbzoo"
 
-rm -r `$JOOMLA`/components/com_jbzoo
-ln -s `$ROOT`/src/joomla-plugins/pkg_jbzoocck/packages/com_jbzoo/site       \
-      `$JOOMLA`/components/com_jbzoo
+rm -r "$JOOMLA/components/com_jbzoo"
+ln -s "$ROOT/src/joomla-plugins/pkg_jbzoocck/packages/com_jbzoo/site"       \
+      "$JOOMLA/components/com_jbzoo"
 
 
 echo ""
 echo ">>> >>> Wordpress: Create symlinks"
-rm -r `$WORDPRESS`/wp-content/plugins/jbzoocck
-ln -s `$ROOT`/src/wordpress-plugin/jbzoocck                                 \
-      `$WORDPRESS`/wp-content/plugins/jbzoocck
+rm -r "$WORDPRESS/wp-content/plugins/jbzoocck"
+ln -s "$ROOT/src/wordpress-plugin/jbzoocck"                                 \
+      "$WORDPRESS/wp-content/plugins/jbzoocck"
