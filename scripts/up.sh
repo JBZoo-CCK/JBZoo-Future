01d954baadae5dda53d4187f5466906be13fb54a
smetdenis <admin@jbzoo.com>
2016-06-15 22:26:49 +0700

    Quick update script

--- scripts/up.sh
+++ scripts/up.sh
@@ -16,6 +16,28 @@
 WORKING_DIR="./src/cck"
 
 echo ">>> >>> Composer: Update"
+composer config bin-dir     "../../bin"     --working-dir=$WORKING_DIR
+composer config vendor-dir  "../../vendor"  --working-dir=$WORKING_DIR
 composer update                 \
     --working-dir=$WORKING_DIR  \
     --optimize-autoloader
+
+
+echo ""
+echo ">>> >>> NPM: Install"
+NODE_ENV=development npm install
+
+
+echo ""
+echo ">>> >>> Bower: Update"
+NODE_ENV=development ./node_modules/.bin/bower update
+
+
+echo ""
+echo ">>> >>> Gulp: Update"
+NODE_ENV=development ./node_modules/.bin/gulp update
+
+
+echo ""
+echo ">>> >>> Webpack"
+NODE_ENV=development ./node_modules/.bin/webpack -v
